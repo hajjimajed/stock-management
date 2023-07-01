15002451d3bdda898f4611ac1b7de0c78b98f094
majed hajji <hajjimajed78@gmail.com>
2023-07-01 22:48:10 +0100

    adding styles to home

--- routes/home/home.component.jsx
+++ routes/home/home.component.jsx
@@ -1,8 +1,13 @@
 import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Dimensions } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth.context';
+
+const { width, height } = Dimensions.get('window');
+
 
 const Home = () => {
     const navigation = useNavigation();
@@ -59,14 +64,12 @@ const Home = () => {
                 </>
             ) : (
                 <>
-                    <SafeAreaView>
-                        <TouchableOpacity onPress={navigateToSignup}>
-                            <Text>Signup</Text>
+                    <SafeAreaView style={styles.container}>
+                        <TouchableOpacity onPress={navigateToSignup} style={styles.button}>
+                            <Text style={styles.buttonText}>Signup</Text>
                         </TouchableOpacity>
-                    </SafeAreaView>
-                    <SafeAreaView>
-                        <TouchableOpacity onPress={navigateToSignin}>
-                            <Text>Signin</Text>
+                        <TouchableOpacity onPress={navigateToSignin} style={styles.button}>
+                            <Text style={styles.buttonText}>Signin</Text>
                         </TouchableOpacity>
                     </SafeAreaView>
 
@@ -75,5 +78,29 @@ const Home = () => {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: width,
+        height: height,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        width: '80%',
+        height: 50,
+        backgroundColor: '#007AFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderRadius: 10,
+        elevation: 5,
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+});
 
 export default Home;
